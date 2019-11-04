5c19049e479c20bb1cd809037bed972a63014c41
brunasqz <bsqueiroz98@gmail.com>
2019-11-04 17:16:08 -0300

    ajuste dados para SVM e dados desbalanceados

--- main.R
+++ main.R
@@ -31,7 +31,7 @@ data <- read.csv("database.csv", sep= ",")
 #=======================================#
 #SMOTE
 data$X0 <- factor(ifelse(data$X0 == 1, "rare", "common"))
-data_smoted <- SMOTE(X0 ~ ., data, perc.over = 300, k = 5, leaner = NULL)
+data_smoted <- SMOTE(X0 ~ ., data, perc.over = 100, k = 5, leaner = NULL)
 
 #Separação dos dados 
 ic0 <- which(data_smoted$X0 == "common")
@@ -50,18 +50,53 @@ xrtest  <- traintest2[[1]]
 
 
 #Aplicação do SVM
-svmtrain <- ksvm(rbind(xctrain[,1:8],xrtrain[,1:8]), 
+svmtrain <- ksvm(as.matrix(rbind(xctrain[,1:8],xrtrain[,1:8])), 
                  c(xctrain[,9],xrtrain[,9]), 
-                 type='C-bsvc', kernel='rbfdot', kpar=(list(2)), C= 5)
+                 type='C-bsvc', kernel='rbfdot', kpar=(list(2)), C= 0.5)
 
 yhat <- predict(svmtrain,
-                rbind(xctest[,1:8],xrtest[,1:8]),
+                as.matrix(rbind(xctest[,1:8],xrtest[,1:8])),
                 type="response")
 
 #Curva ROC
-ROC <- multiclass.roc(rbind(xctest[,1:8],xrtest[,1:8]), yhat)
+ROC <- multiclass.roc(c(xctest[,9],xrtest[,9]), yhat)
 AUC <- auc(ROC)
+
+rs <- ROC[['rocs']]
+plot.roc(rs[[1]])
 
 #=======================================#
 #    Classificação desbalanceando       #
 #=======================================#
+
+data <- read.csv("database.csv", sep= ",")
+ic0 <- which(data$X0 == 0)
+ic1 <- which(data$X0 == 1)
+
+x_com <- data[ic0,]
+x_rare <- data[ic1,]
+
+traintest <- treinaTeste(0.3, x_com)
+xctrain <- traintest[[2]]
+xctest <- traintest[[1]]
+
+traintest2 <- treinaTeste(0.3, x_rare)
+xrtrain <- traintest2[[2]]
+xrtest  <- traintest2[[1]]
+
+
+#Aplicação do SVM
+svmtrain <- ksvm(as.matrix(rbind(xctrain[,1:8],xrtrain[,1:8])), 
+                 c(xctrain[,9],xrtrain[,9]), 
+                 type='C-bsvc', kernel='rbfdot', kpar=(list(2)), C= 0.5)
+
+yhat <- predict(svmtrain,
+                as.matrix(rbind(xctest[,1:8],xrtest[,1:8])),
+                type="response")
+
+#Curva ROC
+ROC <- multiclass.roc(c(xctest[,9],xrtest[,9]), yhat)
+AUC <- auc(ROC)
+
+rs <- ROC[['rocs']]
+plot.roc(rs[[1]])
